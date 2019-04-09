3c9011c7df286edb64611d854fd9fdb357bcd7c4
陈丕显 <chenpixian@chenpixiandeMacBook-Pro.local>
2019-04-09 16:05:30 +0800

    load

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-var btn = $('.begin-btn');
-var num = $('.num');
-var n = 0;
-var flag = true
-setInterval(function(){
-    if(flag){
-        n ++
-    }
-    num.html(n);
-},2)
+// var btn = $('.begin-btn');
+// var num = $('.num');
+// var n = 0;
+// var flag = true
+// setInterval(function(){
+//     if(flag){
+//         n ++
+//     }
+//     num.html(n);
+// },2)
 
-btn.click(function(){
-    $('.ani').addClass('hide');
-    flag = !flag
-    if(n%3){
-        n = 250;
-        $('.ani').removeClass('hide');
-    }
-})
+// btn.click(function(){
+//     $('.ani').addClass('hide');
+//     flag = !flag
+//     if(n%3){
+//         n = 250;
+//         $('.ani').removeClass('hide');
+//     }
+// })
